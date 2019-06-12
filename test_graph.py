27dc29a1630c44d9f23d64012cfede5c38826ebd
Bob Follek <bfollek@gmail.com>
2019-06-12 06:00:26 -0400

    Added test using objects with attributes ATP

--- test_graph.py
+++ test_graph.py
@@ -1,5 +1,6 @@
 # This runs from simple_graph
 
+from functools import total_ordering
 from data_structures.simple_graph.graph import Digraph, Graph
 
 CITIES = ["Seattle", "San Francisco", "Los Angeles", "Riverside", "Phoenix", "Chicago", "Boston", "New York",
@@ -68,3 +69,99 @@ def test_digraph():
     assert sorted(g.neighbors_for_vertex("Atlanta")) == [
         'Chicago', 'Miami', 'Washington']
     assert sorted(g.neighbors_for_vertex("Washington")) == []
+
+##########################################
+# Test with objects that have attributes
+##########################################
+
+
+# https://stackoverflow.com/questions/8796886/is-it-safe-to-just-implement-lt-for-a-class-that-will-be-sorted
+
+@total_ordering
+class City():
+    def __init__(self, name):
+        self._name = name.strip()
+
+    @property
+    def name(self):
+        return self._name
+
+    def str(self):
+        return self.name
+
+    @property
+    def _normalized_name(self):
+        """For comparisons"""
+        return self.name.lower()
+
+    def __eq__(self, other):
+        return self._normalized_name == other._normalized_name
+
+    def __lt__(self, other):
+        return self._normalized_name < other._normalized_name
+
+
+SEATTLE = City("Seattle")
+SAN_FRANCISCO = City("San Francisco")
+LOS_ANGELES = City("Los Angeles")
+RIVERSIDE = City("Riverside")
+PHOENIX = City("Phoenix")
+CHICAGO = City("Chicago")
+BOSTON = City("Boston")
+NEW_YORK = City("New York")
+ATLANTA = City("Atlanta")
+MIAMI = City("Miami")
+DALLAS = City("Dallas")
+HOUSTON = City("Houston")
+DETROIT = City("Detroit")
+PHILADELPHIA = City("Philadelphia")
+WASHINGTON = City("Washington")
+
+CITY_OBJS = [SEATTLE, SAN_FRANCISCO, LOS_ANGELES,
+             RIVERSIDE, PHOENIX, CHICAGO, BOSTON, NEW_YORK, ATLANTA, MIAMI, DALLAS, HOUSTON, DETROIT, PHILADELPHIA, WASHINGTON]
+
+
+def _add_obj_edges(g):
+    g.add_edge_by_vertices(SEATTLE, CHICAGO)
+    g.add_edge_by_vertices(SEATTLE, SAN_FRANCISCO)
+    g.add_edge_by_vertices(SAN_FRANCISCO, RIVERSIDE)
+    g.add_edge_by_vertices(SAN_FRANCISCO, LOS_ANGELES)
+    g.add_edge_by_vertices(LOS_ANGELES, RIVERSIDE)
+    g.add_edge_by_vertices(LOS_ANGELES, PHOENIX)
+    g.add_edge_by_vertices(RIVERSIDE, PHOENIX)
+    g.add_edge_by_vertices(RIVERSIDE, CHICAGO)
+    g.add_edge_by_vertices(PHOENIX, DALLAS)
+    g.add_edge_by_vertices(PHOENIX, HOUSTON)
+    g.add_edge_by_vertices(DALLAS, CHICAGO)
+    g.add_edge_by_vertices(DALLAS, ATLANTA)
+    g.add_edge_by_vertices(DALLAS, HOUSTON)
+    g.add_edge_by_vertices(HOUSTON, ATLANTA)
+    g.add_edge_by_vertices(HOUSTON, MIAMI)
+    g.add_edge_by_vertices(ATLANTA, CHICAGO)
+    g.add_edge_by_vertices(ATLANTA, WASHINGTON)
+    g.add_edge_by_vertices(ATLANTA, MIAMI)
+    g.add_edge_by_vertices(MIAMI, WASHINGTON)
+    g.add_edge_by_vertices(CHICAGO, DETROIT)
+    g.add_edge_by_vertices(DETROIT, BOSTON)
+    g.add_edge_by_vertices(DETROIT, WASHINGTON)
+    g.add_edge_by_vertices(DETROIT, NEW_YORK)
+    g.add_edge_by_vertices(BOSTON, NEW_YORK)
+    g.add_edge_by_vertices(NEW_YORK, PHILADELPHIA)
+    g.add_edge_by_vertices(PHILADELPHIA, WASHINGTON)
+
+
+def _make_obj_digraph():
+    city_obj_digraph = Digraph(CITY_OBJS)
+    _add_obj_edges(city_obj_digraph)
+    return city_obj_digraph
+
+
+def test_obj_digraph():
+    g = _make_obj_digraph()
+    assert sorted(g.neighbors_for_vertex(SEATTLE)) == [
+        CHICAGO, SAN_FRANCISCO]
+    assert sorted(g.neighbors_for_vertex(SAN_FRANCISCO)) == [
+        LOS_ANGELES, RIVERSIDE]
+    assert sorted(g.neighbors_for_vertex(ATLANTA)) == [
+        CHICAGO, MIAMI, WASHINGTON]
+    assert sorted(g.neighbors_for_vertex(WASHINGTON)) == []
